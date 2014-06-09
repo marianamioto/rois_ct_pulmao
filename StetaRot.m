f47b602a3ebbda50ae010dcc48670fb002491c4d
Mariana Mioto <mariana.mioto@gmail.com>
2014-06-08 21:42:42 -0300

    Gambiarra pra fazer para os proximos octantes a tempo de assistir Game of Thrones

--- StetaRot.m
+++ StetaRot.m
@@ -23,8 +23,9 @@ ang = 5;
 tam = 64;
 iters = 18;
 ultima_soma = 0;
-somas = zeros(iters, 1);
+somas = zeros(iters*2, 1);
 metade = tam / 2;
+
 
 %for para o loop que armazena o conjunto de pontos de cada angulo
 for n = 1:iters
@@ -58,8 +59,44 @@ for n = 1:iters
     ultima_soma = soma(xs,ys, ultima_soma, mag);
     somas(n) = ultima_soma;
    
-     
-    ang = ang + 5;
-     
-     
+    ang = ang + 5;     
+end
+
+
+ang = 0;
+mag = rot90(mag, -1);
+
+%for para o loop que armazena o conjunto de pontos de cada angulo
+for n = 1:iters
+    %hipotenusa para angulo até 45 graus 
+    ca = metade;
+    hip  = ca/(cosd(ang));
+    co = sind(ang)*hip;
+    
+    %encontra os pontos para até 45 graus
+    if ang <= 45
+        phx = tam;
+        if (metade - co) < 1
+            phy = 1;
+        else
+            phy = metade - co;
+        end
+    else
+        phy = 1;
+        if (metade + co) > tam
+            phx = tam;
+        else
+            phx = metade + co;
+        end
+    end
+
+        
+    %traça a reta que vai do ponto inicial até os pontos encontrados em x e y
+    [xs ys] = encontrapontos(pix,piy,pfy,phx,phy);
+    
+    %coloca os pontos encontrados pelo algoritmo de bresenham na struct
+    ultima_soma = soma(xs,ys, ultima_soma, mag);
+    somas(n+18) = ultima_soma;
+   
+    ang = ang + 5;     
 end
