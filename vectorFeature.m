4e16397bf7f8f70ee7b6ce8d710474a54f86f9a7
Mariana Mioto <mariana.mioto@gmail.com>
2014-06-09 13:26:23 -0300

    adicionando metodos

--- vectorFeature.m
+++ vectorFeature.m
@@ -1,25 +1,27 @@
 % Nome dos arquivos com extensao .jpg localizados no diretorio recebido
     
-    directory = 'C:\Users\Marcela\Desktop\TCC\Rois_CT_Pulmao\Rois_CT_Pulmao'
-    dirOutput = dir(fullfile(directory,'*.jpg'));
-    fileNames = {dirOutput.name}';    
+     directory = 'C:\Users\Marcela\Desktop\TCC\Rois_CT_Pulmao\Rois_CT_Pulmao'
+     dirOutput = dir(fullfile(directory,'*.jpg'));
+     fileNames = {dirOutput.name}';    
     
     
-    % caminho do arquivo{i} localizado no diretorio
-        filePath = strcat(directory,'\',fileNames{i});
-        % leitura da imagem
-        image = imread(filePath);
+     % caminho do arquivo{i} localizado no diretorio
+     filePath = strcat(directory,'\',fileNames{i});
+     % leitura da imagem
+     imagem = imread(filePath);
+     
+     %magnitude da tranformada que foi aplicada a imagem
+     %calcula a parte real da transformada de fourier
+     mag = abs((fftshift(fft2(imagem))));
+
+     
         
-     %chamada das minhas funções, 
-     %função que encontra pontos para fazer o somatorio de 
-     %de 0 até 180 graus
-     
-     
+     %função que encontra pontos para fazer o somatorio de de 0-180 graus
+     soma180 = StetaRot(mag);
+     soma180Acumulada = somAcumulada(soma180);
      
      %função que encontra pontos que estao no raio de 0 até 31
-     
-     
-     %fazer a somatoria para cada imagem que eu tenho, e 
+     raiosSomados = StetaR(mag);
      
      %gerar histograma para cada vetor de somatorios
      
