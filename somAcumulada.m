1cd080091c4ffe7199309e7b567be81ccfa32aab
Mariana Mioto <mariana.mioto@gmail.com>
2014-06-09 10:54:04 -0300

    funcao que faz a soma acumulada

---
+++ somAcumulada.m
@@ -0,0 +1,7 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%função que cria os resultados com a soma acumulada
+
+
+function acumulada = somAcumulada(somas)
+    acumulada = cumsum(somas);
+end
